794afb02cc3134f8928e97b1e7bc9a5de52e7478
Arthur-Saradjzada <149606895+Arthur-Saradjzada@users.noreply.github.com>
2026-06-11 20:00:11 +0200

    Update 1. spx_data_to_sql.py

--- 1. spx_data_to_sql.py
+++ 1. spx_data_to_sql.py
@@ -18,7 +18,6 @@ Run:
 
 
 Before running:
-
 1. Make sure CONNECTION_STRING is correct for your MySQL setup.
 2. Make sure ROOT_FOLDER points to the correct folder containing the .txt files.
 3. Make sure the MySQL database specified in CONNECTION_STRING exists.
